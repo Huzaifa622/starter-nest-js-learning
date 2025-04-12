3b5256dc9ae209643a06b3dcea8e1c78db59373d
huzaifa <thuzaifa07@gmail.com>
2025-04-12 16:26:17 +0500

    huzaifa

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,22 +1,37 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
-import { posix } from 'node:path/win32';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
+  @Get()
+  findAll() {
+    return [];
+  }
 
-    @Get()
-    findAll(){
-        return []
-    }
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return { id };
+  }
 
-    @Get(':id')
-    findOne(@Param("id") id:string ){
-        return {id}
-    }
+  @Post()
+  create(@Body() user: {}) {
+    return user;
+  }
 
-    @Post()
-    create(@Body() user : {}){
-         return user
-    }
-    
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() user: {}) {
+    return { id, ...user };
+  }
+
+  @Delete(':id')
+  delete(@Param('id') id: string) {
+    return { id };
+  }
 }
