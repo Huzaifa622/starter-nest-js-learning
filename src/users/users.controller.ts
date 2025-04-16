8c329cff39823757028fbf5e01948cde1087ece1
huzaifa <thuzaifa07@gmail.com>
2025-04-16 21:05:49 +0500

    fix

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -8,17 +8,22 @@ import {
   Post,
   Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
+
+  constructor(private readonly userService:UsersService){}
+
+  
   @Get()
   findAll(@Query("role") role? : "admin" | "vendor" | "user") {
-    return role;
+    return this.userService.findAll(role);
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return { id };
+    return this.userService.findOne(+id);
   }
 
   @Post()
